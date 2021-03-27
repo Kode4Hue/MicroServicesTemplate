d4637c03aff3e6c0d1ed3c080b5a0db8c78820e6
Hubert Graham <hubert.graham90@gmail.com>
2021-03-27 18:25:22 -0500

    added mediatr to base api controller

---
+++ MicroServicesTemplate.CoreAPI.Presn/Features/Common/APIControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroServicesTemplate.CoreAPI.Presn.Features.Common
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class APIControllerBase : ControllerBase
+    {
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
